d7a911d6ee19cad3990abdcfc5616025525f240c
Mihail Nephedov <Nephedov93@gmail.com>
2022-12-11 20:18:17 +0300

    update CardOrderDeliveryTest.class

--- src/test/java/ru/netology/CardOrderDeliveryTest.java
+++ src/test/java/ru/netology/CardOrderDeliveryTest.java
@@ -1,7 +1,5 @@
 package ru.netology;
 
-
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
